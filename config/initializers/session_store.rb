f5657ba16e2d444469c94b2839df907c9982f0b7
thinkAmi <dev.thinkami@gmail.com>
2026-02-02 19:59:21 +0900

    feat: add cluster support for session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,11 @@
 Rails.application.config.session_store :cache_store,
                                        cache: ActiveSupport::Cache::RedisCacheStore.new(
-                                         url: "redis://redis-master:6379/0",
+                                         redis: Redis::Cluster.new(nodes: %w[
+                                           redis://redis-cluster-master-1:6379
+                                           redis://redis-cluster-master-2:6379
+                                           redis://redis-cluster-master-3:6379
+                                         ]),
+                                         namespace: "my_namespace"
                                        ),
                                        expire_after: 90.minutes,
-                                       key: "_redis_session"
+                                       key: "_redis_cluster_session"
